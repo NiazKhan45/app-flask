4bb5b52f53759219b24843ac7e98d10f15221f71
Niaz Khan <niazahmadkhan45@gmail.com>
2022-12-01 20:18:06 +0500

    App Flask

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 
 
+from flask import Flask, request
 
 
-#rtyuiop
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+   print (request.headers)
+   return "Hello World!"
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+
+   app.run()
